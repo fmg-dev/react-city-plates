12f82f32453e8710bf03c09461ed993f542bb466
Fatih mehmet Gün <fmggmf01@hotmail.com>
2022-05-25 08:47:22 +0300

    end

---
+++ src/Components/CityPlate.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import "./cityPlate.css";
+import { FormLabel, NativeSelect } from "@mui/material";
+import plates from "../plates";
+
+function CityPlate() {
+  const [newPlate, setNewPlate] = useState();
+
+  function handlePlate(e) {
+    setNewPlate(e.target.value);
+  }
+
+  return (
+    <Box className="box">
+      <div className="formLabel">
+        <FormLabel className="formLabel">
+          <h3>City Plate:</h3>
+          {newPlate}
+        </FormLabel>
+      </div>
+      <FormControl className="form">
+        <InputLabel id="demo-simple-select-label">City Plate</InputLabel>
+        <NativeSelect
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="City Plate"
+          onChange={handlePlate}
+        >
+          {plates.data.map((plate) => (
+            <option key={plate.id} value={plate.il_adi}>
+              {plate.plaka_kodu}
+            </option>
+          ))}
+        </NativeSelect>
+      </FormControl>
+    </Box>
+  );
+}
+
+export default CityPlate;
